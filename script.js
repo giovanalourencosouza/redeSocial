9f9f1464077fa721871543183448fab51baec1cc
giovanalourencosouza <giovana.lourenco.souza@escola.pr.gov.br>
2026-08-13 08:47:46 -0300

    ok

---
+++ script.js
@@ -0,0 +1,11 @@
+//Botao de curtidas
+document.addEventListener("DOMContentLoaded", () =>{ 
+    const LikeBtn = document.querySelector(".left-actions .action-btn:first-child");
+    if(!LikeBtn) return;
+    const likeSvg = likeBtn.querySelector("svg");
+
+    \\localiza o contador
+
+    let textNode = Array 
+})
+
